dc44d0edfc163b19c0dbdfbdbca54ac05b9798a4
Mahedi Azad <mazadcse@gmail.com>
2023-10-06 12:01:26 +0600

    project init of backend and frontend

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,64 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width,initial-scale=1',
+      title: 'icePot',
+      titleTemplate: '%s - icePot',
+      meta: [{ name: 'description', content: 'icePot.xyz' }],
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+
+  sitemap: {
+    strictNuxtContentPaths: true,
+  },
+  site: {
+    url: 'http://icepot.xyz',
+    identity: {
+      type: 'Person',
+    },
+    twitter: '@qdnvubp',
+  },
+
+  typescript: {
+    strict: true,
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: [
+        '/',
+      ],
+    },
+  },
+
+  colorMode: {
+    classSuffix: '',
+    preference: 'light',
+    fallback: 'dark',
+  },
+
+  modules: [
+    'nuxt-icon',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    '@nuxt/content',
+    '@nuxtjs/robots',
+    '@nuxtseo/module',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/color-mode',
+    'nuxt-simple-sitemap',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  content: {
+    highlight: {
+      theme: 'dracula',
+    },
+  },
+})
